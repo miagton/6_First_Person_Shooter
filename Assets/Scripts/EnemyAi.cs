0c96d0914e16be356a0b1556d843164612725481
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-30 19:49:23 +0300

    NAvmesh+sandbox+gun

---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform player = null;
+    [SerializeField] float agroRadius = 20f;
+
+
+    float distanceToPlayer = Mathf.Infinity;
+    NavMeshAgent agent;
+    bool isProvoked = false;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (isProvoked)
+        {
+            EngadgeTarget();
+        }
+        else if(distanceToPlayer <= agroRadius)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngadgeTarget()
+    {
+        if (distanceToPlayer > agent.stoppingDistance)
+        {
+            Chase();
+
+        }
+        if (distanceToPlayer <= agent.stoppingDistance)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        print("Attacking" + player.name);
+    }
+
+    private void Chase()
+    {
+        isProvoked = true;
+        agent.SetDestination(player.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, agroRadius);
+    }
+}
