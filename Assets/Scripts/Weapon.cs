5da931ba2f0fead45876df69074d0649ddcec387
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-07-01 10:50:20 +0300

    Raycast for shooting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float weaponRange = 100f;
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, weaponRange))
+        {
+         print("I hit :" + hit.collider.name);
+
+        }
+        
+
+    }
+}
