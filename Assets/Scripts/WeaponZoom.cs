cc52ff9915c3308ba8d12b892196a076a99d1303
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-07-01 22:40:27 +0300

    weapon zoom added

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera = null;
+    
+    [Tooltip("Less number=>more zoom")]
+    [SerializeField] float zoomPower = 20f;
+    [SerializeField]float standartZoom=60f;
+    
+    bool isZoomed = false;
+
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ProccesZooming();
+        }
+    }
+
+    private void ProccesZooming()
+    {
+        if (!isZoomed)
+        {
+            isZoomed = true;
+            fpsCamera.fieldOfView = zoomPower;
+            
+        }
+        else
+        {
+            isZoomed = false;
+            fpsCamera.fieldOfView = standartZoom;
+        }
+    }
+}
