0eea60cb95f0022f0995977691566b20e50f9e94
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-07-02 16:36:35 +0300

    flashlight+ battery pickUp

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField] float restoreIntensity = 8f;
+    [SerializeField] float restoreAngle = 40f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponentInChildren<FlashLIght>().RestoreLight(restoreIntensity, restoreAngle);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FlashLIght.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLIght : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light myLight;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    
+    void Update()
+    {
+        DecayLight(lightDecay,angleDecay);
+    }
+
+    private void DecayLight(float light,float angle)
+    {
+        myLight.intensity -= light * Time.deltaTime;
+
+        if (myLight.spotAngle >= minAngle)
+        {
+            myLight.spotAngle -= angle * Time.deltaTime;
+        }
+    }
+    public void RestoreLight(float light, float angle)
+    {
+        myLight.intensity = light;
+        myLight.spotAngle = angle;
+    }
+}
